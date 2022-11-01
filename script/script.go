50ef8865cd16e6b4ac9a8a50012206fdffc354ed
icps0610 <icps0610@gmail.com>
2022-11-01 21:17:06 +0800

    2022/11/01-21:17

--- script/script.go
+++ script/script.go
@@ -7,26 +7,31 @@ import (
 func ArrCompare(arr1, arr2 []string) ([]config.Data, []config.Data, []config.Data, []string) {
     var diff1, diff2, all []config.Data
     var same []string
+    diff1, all, same = compare(arr1, arr2, all, "1")
+    diff2, all, _ = compare(arr2, arr1, all, "2")
+    BubbleSort(all)
+    return diff1, diff2, all, same
+}
+
+func compare(arr1, arr2 []string, all []config.Data, compare string) ([]config.Data, []config.Data, []string) {
+    var diff []config.Data
+    var same []string
 
     for idx, ele := range arr1 {
         if arrIncludeIndex(arr2, ele) == -1 {
-            data := config.Data{idx + 1, ele, ""}
+            var data config.Data
+            if compare == "1" {
+                data = config.Data{idx + 1, ele, ""}
+            } else {
+                data = config.Data{idx + 1, "", ele}
+            }
             all = append(all, data)
-            diff1 = append(diff1, data)
+            diff = append(diff, data)
         } else {
             same = append(same, ele)
         }
     }
-    for idx, ele := range arr2 {
-        if arrIncludeIndex(arr1, ele) == -1 {
-            data := config.Data{idx + 1, "", ele}
-            all = append(all, data)
-            diff2 = append(diff2, data)
-        }
-    }
-
-    BubbleSort(all)
-    return diff1, diff2, all, same
+    return diff, all, same
 }
 
 // func CheckKB(datas []config.Data) []config.Data {
